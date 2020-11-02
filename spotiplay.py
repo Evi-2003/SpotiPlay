d1db3d25677e3fb741f702cea09d715077d8c5ea
Remco Wammes <69012052+Remco17@users.noreply.github.com>
2020-11-02 22:57:50 +0100

    Cleaned a bit, got the right timestamp for the song.
    Need to figure out how to make VLC play at the value of 'roundedSongTimeStamp'. Tried making the youtube url so that it starts at the specified time. But did not work for vlc as it copies the video /:

--- spotiplay.py
+++ spotiplay.py
@@ -1,19 +1,17 @@
-
-
-# using the spotipy api
 import sys
 import vlc
 import spotipy
 import pafy
+import time
+import math
+import os
 import win32gui
 from spotipy.oauth2 import SpotifyOAuth
 from youtube_search import YoutubeSearch
 import spotipy.util as util
-
-# we need some autothentication, because who the fuck are you.
-#The Client_ID Client_Secret And redrect url needs to be set by yourself, for now.
+# we need some Authentication, because who the fuck are you.
+# The Client_ID Client_Secret And Username needs to be set by yourself, for now.
 username = "-"
-    
 scope = "user-read-currently-playing"
 token = util.prompt_for_user_token(
                            username,
@@ -21,25 +19,39 @@ token = util.prompt_for_user_token(
                            client_id='-',
                            client_secret='-',
                            redirect_uri='http://example.com/callback/')
-if token:
-    sp = spotipy.Spotify(auth=token)
-    results = sp.current_user_playing_track()
-    songName = results["item"]["name"]
-    artistName = results['item']['album']['artists'][0]['name']
-    search = songName + " - " + artistName
-    searchResult = YoutubeSearch(search, max_results=1).to_dict()
-   # print(songName)
-    print(artistName)
-    print(search)
-    print(searchResult[0]['id'])
 
-# Youtube part
-url = 'https://youtube.com/watch?v=' + searchResult[0]['id']
-video = pafy.new(url)
-best = video.getbestvideo()
-media = vlc.MediaPlayer(best.url)
-media.play()
-media.set_fullscreen(True)
-media.audio_set_mute(True)
+def EeverythingActually():
+    #GETTING THE SONG
+    if token:
+        sp = spotipy.Spotify(auth=token)
+        results = sp.current_user_playing_track()
+        songName = results["item"]["name"]
+        artistName = results['item']['album']['artists'][0]['name']
+        search = songName + " - " + artistName
+        searchResult = YoutubeSearch(search, max_results=1).to_dict()
+        print(songName)
+        print(artistName)
+        print(search)
+        print(searchResult[0]['id'] )
+        songTimeStamp = results['progress_ms'] / 1000
+        roundedSongTimeStamp = math.trunc(songTimeStamp)
+        # GETTING THE ACTUAL VIDEO
+        url = 'https://youtube.com/watch?v=' + searchResult[0]['id'] + "&t=" + str(roundedSongTimeStamp) + "s" # This is for selecting the right video
+        print(url)
+        video = pafy.new(url)
+        best = video.getbestvideo()
+        media = vlc.MediaPlayer(best.url)
+        media.play()
+        media.audio_set_mute(True)
+
+        videoLength = (video.length)
+        time.sleep(videoLength)
+        media.stop()
+
+        # Resetting the song to 00:00 so the video should be right on time with the song
+
+
+EeverythingActually()
+
 while True:
      pass
